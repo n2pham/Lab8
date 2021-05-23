5ac15fc4ff119460110f19e4d265dc64b174c60b
Nhan Pham <n2pham@ucsd.edu>
2021-05-23 14:04:54 -0700

    Part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,24 @@
 /**
  * @jest-environment jsdom
  */
+import { pushToHistory } from "../scripts/router.js";
 
+describe("test pushToHistory", () => {
+  test("Settings Case", () => {
+    history = pushToHistory("settings");
+    expect(history.length).toBe(2);
+    expect(history.state.page).toBe("settings");
+  });
+
+  test("entry", () => {
+    history = pushToHistory("entry", 1);
+    expect(history.length).toBe(3);
+    expect(history.state.page).toBe("entry1");
+  });
+
+  test("default", () => {
+    history = pushToHistory("", "");
+    expect(history.length).toBe(4);
+    expect(history.state.page).toBe(undefined);
+  });
+});
